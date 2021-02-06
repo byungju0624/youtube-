26dd6f07a9c7f9744b400525162f5186da4b17fc
정병주 <jeongbyeongju@jeongbyeongjuui-MacBookPro.local>
2021-02-06 14:43:54 +0900

    videoItem and vodeList modified

--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import VideoItem from "../video_item/video_item";
-const VideoList = (props) => (
-  <ul>
-    {props.videos.map((video) => (
-      <VideoItem key={video.id} video={video} />
-    ))}
-  </ul>
-);
+import styles from "./video_list.module.css";
+const VideoList = (props) => {
+  const { videos } = props;
+  console.log(videos);
+  return (
+    <ul className={styles.container}>
+      {videos.map((video) => (
+        <VideoItem key={video.id} video={video} />
+      ))}
+    </ul>
+  );
+};
 
 export default VideoList;
