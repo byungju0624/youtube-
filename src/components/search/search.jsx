d23daa45d2c840d811e6f75dc5df146d9bbd570c
정병주 <jeongbyeongju@jeongbyeongjuui-MacBookPro.local>
2021-02-06 21:53:04 +0900

    search add

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,15 +1,35 @@
 import styles from "./search.module.css";
-import React from "react";
+import React, { useRef } from "react";
 
-const Search = (props) => {
+const Search = ({ onSearch }) => {
+  const inputRef = useRef(); //input의 정보 메모하기 용도
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    console.log(value);
+    onSearch(value);
+  };
+  const handleClick = () => {
+    handleSearch();
+  };
+  const pressKey = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <img className={styles.img} src="/images/youtubeLogo.png" />
       </div>
 
-      <input className={styles.input} type="search" placeholder="Search..." />
-      <button className={styles.button} type="submit">
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="search"
+        placeholder="Search..."
+        onKeyPress={pressKey}
+      />
+      <button className={styles.button} type="submit" onClick={handleClick}>
         <img src="./images/search.png" />
       </button>
     </header>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,23 @@ import styles from "./Main.module.css";
 import { YOUTUBE_API } from "./components/youtube_api/youtube_api";
 function App() {
   const [videos, setVideos] = useState([]); // 비디오를 저장해주는 역할
+  const search = (query) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResult=25&q=${query}type=video&key=${YOUTUBE_API}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log("error", error));
+  };
   console.log(YOUTUBE_API);
   useEffect(() => {
     const requestOptions = {
@@ -23,7 +40,7 @@ function App() {
 
   return (
     <div className={styles.app}>
-      <Search />
+      <Search onSearch={search} />
       <VideoList videos={videos} />
     </div>
   );
