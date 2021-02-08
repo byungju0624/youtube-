f7552d2276e094571c66dc5fc7057ddc760291fe
정병주 <jeongbyeongju@jeongbyeongjuui-MacBookPro.local>
2021-02-08 17:22:28 +0900

    add youtube service

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Main from "./main";
 import "@fortawesome/fontawesome-free/js/all.js";
+import Youtube from "./service/youtube";
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 ReactDOM.render(
   <React.StrictMode>
-    <Main />
+    <Main youtube={youtube} />
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/main.jsx
+++ src/main.jsx
@@ -2,40 +2,19 @@ import React, { useState, useEffect } from "react";
 import VideoList from "./components/video_list/video_list";
 import Search from "./components/search/search";
 import styles from "./Main.module.css";
-import { YOUTUBE_API } from "./components/youtube_api/youtube_api";
-function App() {
+
+function App({ youtube }) {
   const [videos, setVideos] = useState([]); // 비디오를 저장해주는 역할
   const search = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResult=25&q=${query}type=video&key=${YOUTUBE_API}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
   };
-  console.log(YOUTUBE_API);
-  useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
 
-    fetch(
-      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${YOUTUBE_API}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
 
   return (
