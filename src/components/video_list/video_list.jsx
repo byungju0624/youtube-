df5349136f20420f4a71d05f8db678468949a02f
정병주 <jeongbyeongju@jeongbyeongjuui-MacBookPro.local>
2021-02-08 18:37:14 +0900

    add video detail

--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import VideoItem from "../video_item/video_item";
 import styles from "./video_list.module.css";
 const VideoList = (props) => {
-  const { videos } = props;
+  const { videos, onVideoClick, display } = props;
   console.log(videos);
   return (
     <ul className={styles.container}>
       {videos.map((video) => (
-        <VideoItem key={video.id} video={video} />
+        <VideoItem
+          key={video.id}
+          video={video}
+          onVideoClick={onVideoClick}
+          display={display}
+        />
       ))}
     </ul>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
 import VideoList from "./components/video_list/video_list";
 import Search from "./components/search/search";
+import VideoDetail from "./components/video_detail/video_detail";
 import styles from "./Main.module.css";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]); // 비디오를 저장해주는 역할
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
+
   const search = (query) => {
     youtube
       .search(query) //
-      .then((videos) => setVideos(videos));
+      .then((videos) => {
+        setVideos(videos);
+        setSelectedVideo(null);
+      });
   };
 
   useEffect(() => {
@@ -20,7 +30,20 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <Search onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
     </div>
   );
 }
